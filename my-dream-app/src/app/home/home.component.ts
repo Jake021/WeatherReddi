5d1902c24b6972dd651eb9d0115938e8d57fa72e
marcusbehr <markb476@hotmail.com>
2021-03-18 01:13:27 -0500

    Data is on home screen

--- my-dream-app/src/app/home/home.component.ts
+++ my-dream-app/src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-//import * as weatherApi from "weatherApi";
-declare const myTest: any;
 
 @Component({
   selector: 'app-home',
@@ -15,26 +13,59 @@ export class HomeComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
+          { title: 'Current Weather', cols: 1, rows: 2 }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 2, rows: 1 }
+        { title: 'Current Weather', cols: 2, rows: 2 }
       ];
     })
   );
 
-  onClick() {
+  //these load the current temp fields on forecast dashboard
+  //they are populated in getCurrentWeather() function
+  clouds='';
+  dewPoint = ''
+  dt ='';
+  feelsLike='';
+  humidity='';
+  pressure='';
+  sunrise='';
+  sunset='';
+  temp='';
+  visibility='';
 
-    console.log(
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=44.953705&lon=-93.089958&appid=7166823e1e205e712f9c3c6576878966')
-    .then(response => response.json())
-    .then(data => console.log(data)));
-    //myTest();
+  getCurrentWeather() {
+
+      async function get() {
+        let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=44.953705&lon=-93.089958&appid=7166823e1e205e712f9c3c6576878966'
+        let obj = await (await fetch(url)).json();
+        
+        return obj;
+    }
+    var tags;
+    (async () => {
+      tags = await get()
+      console.log(tags) //can remove this later once data is preserved better
+
+      this.clouds = ('Clouds: ' + tags.current.clouds);
+      this.dewPoint = ('Dew Points: ' + tags.current.dew_point);
+      this.dt = ('dt: ' + tags.current.dt);
+      this.feelsLike= ('Feels Like: ' + tags.current.feels_like);
+      this.humidity= ('Humidity: ' + tags.current.humidity);
+      this.pressure= ('Pressure: ' + tags.current.pressure);
+      this.sunrise= ('Sunrise: ' + tags.current.sunrise);
+      this.sunset= ('Sunset: ' + tags.current.sunset);
+      this.temp= ('Temperature: ' + tags.current.temp);
+      this.visibility= ('Visibility: ' + tags.current.visibility);
+
+    })()  
+    
+    this.testFunction();
   }
+
+  testFunction(){console.log("here in test function")}
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
