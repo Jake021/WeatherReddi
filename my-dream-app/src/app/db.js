338be5ba468b321ab63951720da22a725242137f
marcusbehr <markb476@hotmail.com>
2021-04-08 23:33:53 -0500

    Clean Up

--- my-dream-app/src/app/db.js
+++ my-dream-app/src/app/db.js
@@ -1,9 +1,3 @@
-// db.js
-
-// mongodb driver
-
-// import {db, dbName, collectionName} from './server';
-
 const MongoClient = require("mongodb").MongoClient;
 
 const dbConnectionUrl = "mongodb+srv://admin:badresume69@cluster0.zox4n.mongodb.net/WeatherReddi?retryWrites=true&w=majority";
@@ -51,48 +45,4 @@ db.initialize(dbName, collectionName, function(dbCollection) { // successCallbac
     throw (err);
 });
 
-
-// var mongodb= require('mongodb');
-// var MongoClient= mongodb.MongoClient;
-// var URL = "mongodb+srv://admin:badresume69@cluster0.zox4n.mongodb.net/WeatherReddi?retryWrites=true&w=majority";
-
-// var db;
-// var error;
-// var waiting = []; // Callbacks waiting for the connection to be made
-
-// MongoClient.connect(URL,function(err,database){
-//   error = err;
-//   db = database;
-
-//   waiting.forEach(function(callback) {
-//     callback(err, database);
-//   });
-// });
-
-// module.exports = function(callback) {
-//   if (db || error) {
-//     callback(error, db);
-//   } else {
-//     waiting.push(callback);
-//   }
-// }
-
-// var db = require('./db');
-
-// router.post('/',function(req,res,next){
-//   username=req.body.username;
-//   password=req.body.password;
-
-//   db.conn(function(err, database) {
-//     if (err) {
-//       res.sendStatus(500);
-//       console.log(err);
-//       return;
-//     }
-
-//     database.collection('profiles').findOne({'Extreme':username}, function(err, docs){
-//       console.log('here');
-//     });
-//   });
-// });
 
