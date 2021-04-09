3ac47970f19e60756e761d2ce8501eb5e2783e13
Cody Curtis <cody.a.curtis@gmail.com>
2021-04-08 23:02:17 -0500

    Adding mongodb connectivity
    Doens't work yet

---
+++ my-dream-app/src/app/server.js
@@ -0,0 +1,22 @@
+// server.js
+
+const express = require("express");
+const server = express();
+
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 4000;
+
+// << db setup >>
+const db = require("./db");
+const dbName = "weatherReddi";
+const collectionName = "profiles";
+
+// << db init >>
+
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
