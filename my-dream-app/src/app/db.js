52b8369fdee67afd63ff5fccdb666182402d02ea
Cody Curtis <cody.a.curtis@gmail.com>
2021-04-16 01:21:12 -0500

    more db connection stuff
    only able to access db by running node db.js

--- my-dream-app/src/app/db.js
+++ my-dream-app/src/app/db.js
@@ -1,48 +1,33 @@
-const MongoClient = require("mongodb").MongoClient;
+const {MongoClient} = require('mongodb');
 
-const dbConnectionUrl = "mongodb+srv://admin:badresume69@cluster0.zox4n.mongodb.net/WeatherReddi?retryWrites=true&w=majority";
-
-function initialize(
-    dbName,
-    dbCollectionName,
-    successCallback,
-    failureCallback
-) {
-    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
-        if (err) {
-            console.log(`[MongoDB connection] ERROR: ${err}`);
-            failureCallback(err); // this should be "caught" by the calling function
-        } else {
-            const dbObject = dbInstance.db(dbName);
-            const dbCollection = dbObject.collection(dbCollectionName);
-            console.log("[MongoDB connection] SUCCESS");
-
-            successCallback(dbCollection);
-        }
-    });
+async function main() {
+  const uri = "mongodb+srv://admin:badresume69@cluster0.zox4n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    await listDatabases(client);
+    await findOneListingByName(client, "Sunny Days");
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
 }
 
-module.exports = {
-    initialize
+main().catch(console.error);
+
+async function listDatabases(client) {
+  const dataBasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  dataBasesList.databases.forEach(db => console.log(` - ${db.name} `));
 };
 
-
-// << db init >>
-const db = require("./db");
-const dbName = "weatherReddi";
-const collectionName = "profiles";
-
-db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
-    // get all items
-    dbCollection.find().toArray(function(err, result) {
-        if (err) throw err;
-          console.log(result);
-    });
-
-    // << db CRUD routes >>
-
-}, function(err) { // failureCallback
-    throw (err);
-});
-
-
+async function findOneListingByName(client, nameOfListing) {
+  const result = await client.db("weatherReddi").collection("profiles").findOne({ profile_Name: nameOfListing});
+  if (result) {
+    console.log(`Found Data '${nameOfListing}':`);
+    console.log(result);
+  } else {
+    console.log(`No listings found for '${nameOfListing}'`);
+  }
+}
